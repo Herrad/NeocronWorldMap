19911c36fba588656428b75db77cf98a55b6bd50
Herrad <herrad.koe@gmail.com>
2013-12-07 14:18:00 +0000

    zone details view model builder returns an empty view model

---
+++ NeocronWorldMap.Web/ViewModelBuilders/ZoneDetailsViewModelBuilder.cs
@@ -0,0 +1,13 @@
+﻿using NeocronWorldMap.Web.Domain;
+using NeocronWorldMap.Web.ViewModels;
+
+namespace NeocronWorldMap.Web.ViewModelBuilders
+{
+    public class ZoneDetailsViewModelBuilder : IBuildZoneDetailsViewModels
+    {
+        public ZoneDetailsViewModel Build(IHaveZoneDetails zone)
+        {
+            return new ZoneDetailsViewModel();
+        }
+    }
+}
---
+++ Test.NeocronWorldMap.Web/ViewModelBuilders/TestZoneDetailsViewModelBuilder.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using NeocronWorldMap.Web.ViewModelBuilders;
+
+namespace Test.NeocronWorldMap.Web.ViewModelBuilders
+{
+    [TestFixture]
+    public class TestZoneDetailsViewModelBuilder
+    {
+        [Test]
+        public void Builds_a_ZoneDetailsViewModel()
+        {
+            var zoneDetailsViewModelBuilder = new ZoneDetailsViewModelBuilder();
+
+            var zoneDetailsViewModel = zoneDetailsViewModelBuilder.Build(null);
+
+            Assert.That(zoneDetailsViewModel, Is.Not.Null);
+        }
+    }
+}
