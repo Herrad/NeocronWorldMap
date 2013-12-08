fbb8784798b2be395fff6341052c9862153cd1b3
Herrad <herrad.koe@gmail.com>
2013-12-08 09:00:47 +0000

    Progressive enhancement done (no more ugly links)

---
+++ NeocronWorldMap.Web/Assets/Scripts/Main.js
@@ -0,0 +1,23 @@
+﻿window.onload = function () {
+    var worldGrid = $('.world-grid'),
+        cells = $('td', worldGrid);
+    
+    removeAnchorsAndBindClickEvents(cells);
+}
+
+function removeAnchorsAndBindClickEvents(cells) {
+
+    cells.each(function (index, cell) {
+        $('a', cell).remove();
+        $(cell).bind('click', cellClicked);
+    });
+}
+
+function cellClicked(cell) {
+    $.ajax({
+        url: "/zone/details?xCoordinate=10&yCoordinate=f",
+        success: function(result) {
+            $('.zone-detail').html(result);
+        }
+    });
+}
