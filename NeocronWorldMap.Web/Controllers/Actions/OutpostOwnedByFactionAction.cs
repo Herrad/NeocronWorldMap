3bfbcc8b096f55a1ce4c59ab8f7b725b5e9a4d3e
Herrad <herrad.koe@gmail.com>
2013-12-17 22:41:42 +0000

    PADDED OUT ACTION

---
+++ NeocronWorldMap.Web/Controllers/Actions/OutpostOwnedByFactionAction.cs
@@ -0,0 +1,10 @@
+﻿namespace NeocronWorldMap.Web.Controllers.Actions
+{
+    public class OutpostOwnedByFactionAction
+    {
+        public void Execute(string xCoordinate, char yCoordinate, IRenderJsonViews jsonViewRenderer)
+        {
+            jsonViewRenderer.SetJsonObject(new { xCoordinates = new[] { "01", "02", "03" }, yCoordinates = new[] { 'a', 'b', 'c' } });
+        }
+    }
+}
---
+++ Test.NeocronWorldMap.Web/Controllers/Action/TestOutpostOwnedByFactionAction.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using NeocronWorldMap.Web.Controllers;
+using NeocronWorldMap.Web.Controllers.Actions;
+using Rhino.Mocks;
+
+namespace Test.NeocronWorldMap.Web.Controllers.Action
+{
+    [TestFixture]
+    public class TestOutpostOwnedByFactionAction
+    {
+        [Test]
+        public void Sets_ObjectToJsonify_on_controller()
+        {
+            var outpostOwnedByFactionAction = new OutpostOwnedByFactionAction();
+
+            var jsonViewRenderer = MockRepository.GenerateMock<IRenderJsonViews>();
+            outpostOwnedByFactionAction.Execute("05", 'f', jsonViewRenderer);
+
+            jsonViewRenderer
+                .AssertWasCalled(
+                    x => x.SetJsonObject(Arg<object>.Is.NotNull),
+                    c => c.Repeat.Once());
+        }
+    }
+}
